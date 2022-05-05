ad10df4c64d25e6b45f94982545f09e79164f929
Tim Schulz <tim@getbej.com>
2022-05-05 07:25:36 +0000

    Add conditional rendering of loading wheel  with CDM

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,22 +2,38 @@ import React, { Component } from "react";
 import { savedPosts } from "../posts.json";
 import css from "./css/Content.module.css";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            isLoaded: false,
+        };
+    }
+
+    getLoaded() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+
+    componentDidMount() {
+        this.getLoaded()
     }
 
     render() {
         return (
             <div>
+            {/* <Loader /> */}
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts}/>
+                    { this.state.isLoaded ? <PostItem savedPosts={savedPosts}/> : <Loader /> }
                 </div>
             </div>
         );
